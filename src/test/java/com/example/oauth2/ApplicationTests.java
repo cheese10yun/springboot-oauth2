3f560b39849bb94d098fbce0dc43825af3de0587
Yun <cheese10yun@gmail.com>
2018-11-12 02:55:05 +0900

    Add Rename

--- src/test/java/com/example/oauth2/ApplicationTests.java
+++ src/test/java/com/example/oauth2/ApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class Oauth2ApplicationTests {
+public class ApplicationTests {
 
     @Test
     public void contextLoads() {
