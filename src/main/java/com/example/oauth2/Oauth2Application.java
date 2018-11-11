45a40859f885e2cefafd58538473aa5ae599b4f6
Yun <cheese10yun@gmail.com>
2018-11-12 02:50:49 +0900

    First commit

---
+++ src/main/java/com/example/oauth2/Oauth2Application.java
@@ -0,0 +1,12 @@
+package com.example.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2Application.class, args);
+    }
+}
